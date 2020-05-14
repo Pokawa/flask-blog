067a0403daf3c8fb55bbe67ca586d26927918fbc
Hubert Cwajda <cwajdahubert@gmail.com>
2020-05-14 20:31:54 +0200

    Formatting and models

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,10 @@ from wtforms.validators import Length, InputRequired
 
 
 class LoginForm(FlaskForm):
-    username = TextField('Username', validators=[Length(min=4, max=20), InputRequired()])
-    password = PasswordField('Password', validators=[Length(min=8, max=80), InputRequired()])
-    remember = BooleanField('Remember me', description='Checkboxes can be tricky.')
+    username = TextField('Username', validators=[Length(min=4, max=20),
+                         InputRequired()])
+    password = PasswordField('Password', validators=[Length(min=8, max=80),
+                             InputRequired()])
+    remember = BooleanField('Remember me',
+                            description='Checkboxes can be tricky.')
     submit_button = SubmitField('Submit Form')
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from .utilities import database
 from flask_login import UserMixin
 
 
+tags = database.Table('tags',
+                      database.Column('article_id', database.Integer, database.ForeignKey('article.id')),
+                      database.Column('tag_id', database.Integer, database.ForeignKey('tag.id')))
+
+
 class Article(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     title = database.Column(database.String(100), nullable=False)
@@ -10,6 +15,7 @@ class Article(database.Model):
     time_posted = database.Column(database.DateTime)
     visible = database.Column(database.Boolean)
     author_id = database.Column(database.Integer, database.ForeignKey('user.id'))
+    tags = database.relationship('Tag', secondary=tags, lazy='subquery', backref=database.backref('pages', lazy=True))
 
 
 class User(UserMixin, database.Model):
@@ -19,3 +25,8 @@ class User(UserMixin, database.Model):
     displayname = database.Column(database.String(80), nullable=False)
     bio = database.column(database.Text)
     articles = database.relationship('Article', backref='author')
+
+
+class Tag(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String(80), nullable=False)
