f87ffee6b0a63bc77bec1aefdce5c2818629bc69
Hubert Cwajda <cwajdahubert@gmail.com>
2020-05-14 18:34:19 +0200

    Removed nav

--- blog/utilities.py
+++ blog/utilities.py
@@ -1,15 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_nav.elements import Navbar, View 
-from flask_nav import Nav
 
 database = SQLAlchemy()
 
 login_manager = LoginManager()
 login_manager.login_view = 'login'
-
-nav = Nav()
-topbar = Navbar('Flask-Blog',
-    View('Latest', 'blog.home'),
-    View('Most popular', 'blog.home'))
-nav.register_element('top', topbar)
