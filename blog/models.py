f059ebead1c02616800da2c02a4700548d716fc3
Hubert Cwajda <cwajdahubert@gmail.com>
2020-05-14 20:12:22 +0200

    Added author foreignkey

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,13 @@ class Article(database.Model):
     text = database.Column(database.Text)
     time_posted = database.Column(database.DateTime)
     visible = database.Column(database.Boolean)
+    author_id = database.Column(database.Integer, database.ForeignKey('user.id'))
 
 
 class User(UserMixin, database.Model):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String(80), nullable=False)
     password = database.Column(database.String(80), nullable=False)
+    displayname = database.Column(database.String(80), nullable=False)
+    bio = database.column(database.Text)
+    articles = database.relationship('Article', backref='author')
